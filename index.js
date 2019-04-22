9cb8b68eb9600ffd10c8779eb3a47472811f381e
Thach Le <thach.le@hdwebsoft.co>
2019-04-22 13:15:02 +0700

    create connect

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('heool');
+var express = require("express");
+var app = express();
+app.use(express.static("./public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+var server = require("http").Server(app);
+var socket = require("socket.io")(server);
+server.listen(3000);
+
+socket.on("connection", function(soc){
+    console.log(111);
+});
+
+app.get("/", function(req, res){
+    res.render("index");
+});

bece97f5cb0112f2101a0abfe8f218675a621e07
Matthew Thach <mtngocthach1202@gmail.com>
2019-04-29 01:35:09 +0700

    update code

--- index.js
+++ index.js
@@ -8,21 +8,26 @@ var server = require("http").Server(app);
 var socket = require("socket.io")(server);
 server.listen(3000);
 
-var arrUser = new Array();
+var arrUser = [];
 
-socket.on("connection", function(soc){
-    // console.log(111);
-    socket.on("client_send_user", function(user){
+socket.on("connection", function (soc) {
+    soc.on("client_send_user", function (user) {
         if (arrUser.indexOf(user) >= 0) {
-            socket.emit("user_exist");
+            soc.emit("login_false");
         } else {
             arrUser.push(user);
-            socket.emit("login_success", user);
+            soc.userName = user;
+            soc.emit("login_success", user);
+            socket.sockets.emit("server_list_user", arrUser);
         }
+    });
+
+    soc.on("user_send_mess", function (mess) {
+        socket.sockets.emit("server_send_chat", { user: soc.userName, mess: mess });
     });
 });
 
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.render("index");
 });
--- public/js/handle.js
+++ public/js/handle.js
@@ -1,15 +1,45 @@
 var socket = io("http://localhost:3000");
-$(document).ready(function(){
+var name_chat = '';
+$(document).ready(function () {
     $("#loginForm").show();
     $("#chatForm").hide();
 
-    $("#btnSubmit").click(function(){
-        socket.emit("client_send_user", $("#txt_user"));
+    $("#btnSubmit").click(function () {
+        socket.emit("client_send_user", $("#user_login").val());
+    });
+
+    $("#btn_chat").click(function () {
+        socket.emit("user_send_mess", $("#txt_chat").val());
+        $("#txt_chat").val("");
+    });
+
+    if(name_chat != ''){
+        $("#loginForm").html('');
+    }
+});
+
+socket.on("login_success", function (data) {
+    $("#current_user").html(data);
+    name_chat = data;
+    $("#form-inline").hide();
+    $("#chatForm").show();
+});
+
+socket.on("login_false", function (data) {
+    alert("User name have used in chatbox");
+});
+
+socket.on("server_list_user", function (data) {
+    $(".list_user").html("");
+    data.forEach(function (element) {
+        $(".list_user").append("<li class='user_in_list'>" + element + "</li>");
     });
 });
 
-socket.on("login_success", function(data){
-    $("#current_name").html(data);
-    $("#loginForm").hide();
-    $("#chatForm").show();
+socket.on("server_send_chat", function (data) {
+    if (name_chat == data.user) {
+        $("#main_chat").append("<div class='w-100 text-right pl-2 pr-2 chat_contain text-white'>" + data.user + ": " + data.mess + "</div>");
+    } else {
+        $("#main_chat").append("<div class='w-100 text-left pl-2 pr-2 chat_contain'>" + data.user + ": " + data.mess + "</div>");
+    }
 });
